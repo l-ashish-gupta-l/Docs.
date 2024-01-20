aba02d3a64a401d84abe152ca651b46cdff1dfb6
l-ashish-gupta-l <ashish3072002@gmail.com>
2024-01-20 17:15:37 +0530

    task form added

---
+++ Client/src/Component/AddForm.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+function AddForm() {
+   
+    return (
+        <div className={`absolute place-items-center w-[40%] h-[90%] top-1/2 shadow-md left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl bg-zinc-200`
+}
+      ></div>  )
+}
+
+export default AddForm
--- Client/src/Component/ForeGround.jsx
+++ Client/src/Component/ForeGround.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react'
 import Card from './Card'
 import axios from 'axios'
+import { IoIosAdd } from "react-icons/io";
+import AddForm from './AddForm';
 axios.defaults.withCredentials = true;
 function ForeGround() {
     const ref = useRef(null)
@@ -18,14 +20,24 @@ function ForeGround() {
         getUserData();
     }, []);
 
+    const [visible, setVisible] = useState(false);
+
+    const toggleForm = () => {
+        setVisible((prevVisible) => !prevVisible);
+    };
+
     return (<>
-        <div ref={ref} className='fixed z-10 top-0 p-10  left-0 w-full h-screen bg-transparent  flex gap-10 '>
-        <div className='absolute px-20 right-0 text-lg' >
+        <div ref={ref} className='absolute z-10 top-0 p-10  left-0 w-full h-screen bg-transparent  flex gap-10 '>
+            <div className='absolute px-20 right-0 text-lg' >
                 <h1 className='text-black font-normal'>Hello, <span className='font-semibold '>{userData && userData.Username}</span> </h1>
             </div>
-        <div className='w-full h-full absolute flex p-5 left-0 top-0  items-end'>    
-            <Card reference={ref} />
+            <div className='w-full h-full absolute flex p-5 left-0 top-0  items-end'>
+                <Card reference={ref} />
             </div>
+            <button onClick={toggleForm} className='absolute right-20  bottom-10 w-14 h-14 rounded-full shadow-inner border-2 flex justify-center items-center bg-black'>
+                <IoIosAdd color='white' size={40} />
+            </button>
+            {visible && <AddForm visibility={visible} />}
         </div>
     </>
     )
