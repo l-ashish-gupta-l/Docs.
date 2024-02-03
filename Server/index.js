a90b65afdb4466b4a69e3885006b82db8c8a450e
l-ashish-gupta-l <ashish3072002@gmail.com>
2024-02-03 18:31:59 +0530

    Host

--- Server/index.js
+++ Server/index.js
@@ -23,7 +23,13 @@ import {
 
 const PORT = process.env.PORT;
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 connectdb();
